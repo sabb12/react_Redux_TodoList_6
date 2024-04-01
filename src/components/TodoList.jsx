72e2dae8348732bfd2c5e7f0697880f4696b18eb
sabb12 <sabb0923@gamil.com>
2024-04-02 00:12:23 +0900

    수정 기능 추가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,33 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useRef, useState } from "react";
-import { create, done, update, remove } from "../store/module/todo";
+import { create, done, remove, update } from "../store/module/todo";
 
 export default function TodoList() {
   const list = useSelector((state) => state.todo.list);
   const todoList = list.filter((todo) => !todo.done);
   const dispatch = useDispatch();
-  const todoRef = useRef();
+  const todoRefs = useRef([]);
+
   const nextID = useSelector((state) => state.todo.nextID);
-  console.log("nextID: ", nextID);
+
+  const [editingTodoId, setEditingTodoId] = useState(null);
+  const [editingText, setEditingText] = useState("");
+
   return (
     <section className="TodoList">
       <h2>오늘 할 일</h2>
       <div>
-        <input type="text" placeholder="Todo" ref={todoRef} />
+        <input
+          type="text"
+          placeholder="Todo"
+          ref={(el) => (todoRefs.current[nextID] = el)}
+        />
         <button
           onClick={() => {
-            dispatch(create({ id: nextID, text: todoRef.current.value }));
-            todoRef.current.value = "";
+            dispatch(
+              create({ id: nextID, text: todoRefs.current[nextID].value })
+            );
+            todoRefs.current[nextID].value = "";
           }}
         >
           추가
@@ -34,7 +44,34 @@ export default function TodoList() {
                   dispatch(done(todo.id));
                 }}
               />
-              <span>{todo.text}</span>
+              {editingTodoId === todo.id ? (
+                <input
+                  type="text"
+                  value={editingText}
+                  onChange={(e) => setEditingText(e.target.value)}
+                />
+              ) : (
+                <span>{todo.text}</span>
+              )}
+              {editingTodoId !== todo.id ? (
+                <button
+                  onClick={() => {
+                    setEditingTodoId(todo.id);
+                    setEditingText(todo.text);
+                  }}
+                >
+                  수정
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    dispatch(update(todo.id, { text: editingText }));
+                    setEditingTodoId(null);
+                  }}
+                >
+                  확인
+                </button>
+              )}
               <button
                 onClick={() => {
                   dispatch(done(todo.id));
@@ -48,13 +85,6 @@ export default function TodoList() {
                 }}
               >
                 삭제
-              </button>
-              <button
-                onClick={() => {
-                  dispatch(update({ id: nextID, text: todoRef.current.value }));
-                }}
-              >
-                수정
               </button>
             </li>
           );
